9602d06ce551c3a634b615863bc7b4dec68358e4
-TokyoGhoul- <0hellnomad0@gmail.com>
2026-07-01 05:21:02 +0300

    add main

---
+++ main.py
@@ -0,0 +1,17 @@
+from src.classes import Aeroplane, AeroplanesAPI, JsonSaver
+from src.utils import user_interaction
+
+country, top_n, filter_words, altitude_range = user_interaction()
+
+response = AeroplanesAPI().get_aeroplanes(country)
+
+aeroplanes = list(Aeroplane.cast_to_object_list(response))  # type: ignore
+
+filtered_aeroplanes = Aeroplane.filter_aeroplanes(aeroplanes, filter_words)
+
+ranged_aeroplanes = Aeroplane.filter_aeroplanes_by_altitude(filtered_aeroplanes, altitude_range)
+
+sorted_aeroplanes = Aeroplane.sort_by_altitude(ranged_aeroplanes)
+top_aeroplanes = Aeroplane.get_top_n_aeroplanes(sorted_aeroplanes, top_n)
+for plane in top_aeroplanes:
+    JsonSaver.add_aeroplane(plane)
